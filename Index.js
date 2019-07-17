1537c360af159db55cd1dec7a1452d6f3ac836db
Feri Krisnanto <ferrykrisnanto312@gmail.com>
2019-07-17 11:33:10 +0700

    penambahan static folder dan file

--- Index.js
+++ Index.js
@@ -3,6 +3,9 @@
 var express = require('express');
 var server = express();
 
+//add public file
+server.use(express.static(__dirname + '/public'));
+
 server.listen(4000, function () {
 	console.log('Server Sudah Jadi');
 });
